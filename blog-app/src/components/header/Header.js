451d28b9175fb55c0a9a6d4cf26643a73ed7ea82
Hani al Sabti <h.sabti87@gmail.com>
2021-10-20 19:21:26 +0200

    created state for blog title

--- blog-app/src/components/header/Header.js
+++ blog-app/src/components/header/Header.js
@@ -1,37 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from '@mui/material/Button';
 import { useStyles } from './HeaderStyles';
 import { NavLink } from 'react-router-dom';
 
 const Header = () => {
-  const classes = useStyles();
+  const [blogTitle, setBlogTitle] = useState(false);
+  const classes = useStyles({ blogTitle });
   return (
     <div className={classes.headerContainer}>
-      <div className={classes.innerContainer}>
-        <div className={classes.logo}></div>
-        <div className={classes.buttonsContainer}>
-          <NavLink
-            exact
-            to='/'
-            activeStyle={{
-              borderBottom: '#E95E30 2px solid',
-            }}
-          >
-            <Button disableRipple className={classes.btn}>
-              home
-            </Button>
-          </NavLink>
-          <NavLink
-            to='/blogs-page'
-            activeStyle={{
-              borderBottom: '#E95E30 2px solid',
-            }}
-          >
-            <Button disableRipple className={classes.btn}>
-              Blog
-            </Button>
-          </NavLink>
+      <div className={classes.headerContainerMask}>
+        <div className={classes.innerContainer}>
+          <div className={classes.logo}></div>
+          <div className={classes.buttonsContainer}>
+            <NavLink
+              exact
+              to='/'
+              activeStyle={{
+                borderBottom: '#E95E30 2px solid',
+              }}
+            >
+              <Button
+                disableRipple
+                className={classes.btn}
+                onClick={() => setBlogTitle(false)}
+              >
+                home
+              </Button>
+            </NavLink>
+            <NavLink
+              to='/blogs-page'
+              activeStyle={{
+                borderBottom: '#E95E30 2px solid',
+              }}
+            >
+              <Button
+                disableRipple
+                className={classes.btn}
+                onClick={() => setBlogTitle(true)}
+              >
+                Blog
+              </Button>
+            </NavLink>
+          </div>
         </div>
+        <div className={classes.blogTitle}>Blog</div>
       </div>
     </div>
   );
