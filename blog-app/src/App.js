c4b2a735c72678f5f8b59f50d03b404e077b57bb
Hani al Sabti <h.sabti87@gmail.com>
2021-10-20 16:45:06 +0200

    Created routes for different pages

--- blog-app/src/App.js
+++ blog-app/src/App.js
@@ -1,5 +1,31 @@
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import CssBaseline from '@mui/material/CssBaseline';
+import { ThemeProvider } from '@mui/material/styles';
+import { theme } from './GlobalStyles';
+import Header from './components/header/Header';
+import LandingPage from './pages/home/LandingPage';
+import Footer from './components/footer/Footer';
+import Blogs from './pages/blogs-page/Blogs';
+import './app.css';
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Router>
+          <div className='main-container'>
+            <Header />
+            <Switch>
+              <Route exact path='/' component={LandingPage} />
+              <Route exact path='/blogs-page' component={Blogs} />
+            </Switch>
+            <Footer />
+          </div>
+        </Router>
+      </ThemeProvider>
+    </>
+  );
 }
 
 export default App;
